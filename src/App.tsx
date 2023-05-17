0c9e17e260e092282c8b69f852fd30d06ee49c98
Coderloff <coderloff@gmail.com>
2023-05-17 13:21:07 +0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Sidebar from "./components/Sidebar"
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 const App = () => {
+  console.log(window.innerWidth);
   return (
     <>
-    <Sidebar/>
-    <Header />
-    <Main coinId="bitcoin"/>
+      <Sidebar />
+      <Header />
+      <Main coinId="bitcoin" />
+      <div className="use-pc">
+        <div className="content">
+          <i className="fa-light fa-desktop icon"></i>
+          <h2>
+            Please use PC for the <span style={{ color: "#6fff7d" }}>best</span>{" "}
+            experience
+          </h2>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
